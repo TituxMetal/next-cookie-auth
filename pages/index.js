21b015be50b5ab4f8e1441f1371c9c849feb7877
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-25 05:44:30 +0200

    Add the front-end home page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Next Cookie Auth</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa debitis, molestiae quos,
+        natus quaerat tempora nostrum excepturi voluptas laudantium, placeat neque unde voluptates.
+        Eveniet, illum! Ipsum dignissimos praesentium in suscipit.
+      </p>
+    </>
+  )
+}
+
+export default Home
