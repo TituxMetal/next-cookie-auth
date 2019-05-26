0be572ab1890cc175abf6cb0c6f976c053cbc50a
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-26 08:26:35 +0200

    Update the Home page; add the Page and Title components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
+import { Page } from '../components/layout'
+import { Title } from '../components/styled'
+
 const Home = () => {
   return (
-    <>
-      <h1>Next Cookie Auth</h1>
+    <Page title='Home Page'>
+      <Title>Next Cookie Auth</Title>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa debitis, molestiae quos,
         natus quaerat tempora nostrum excepturi voluptas laudantium, placeat neque unde voluptates.
         Eveniet, illum! Ipsum dignissimos praesentium in suscipit.
       </p>
-    </>
+    </Page>
   )
 }
 
