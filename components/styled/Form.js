ea897fdef2627313453698290b65c27e7cf6e1ef
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-06-03 09:58:19 +0200

    Add Form styled component

---
+++ components/styled/Form.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 1rem;
+`
+
+export default Form
