a968ec11649c389181804ffe7984d526aeefe2e8
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-06-05 23:27:27 +0200

    Update users Profile component; remove created date because of some errors in browser console

--- components/users/Profile.js
+++ components/users/Profile.js
@@ -4,13 +4,11 @@ import { UserContext } from '../../context'
 
 const Profile = () => {
   const { user } = useContext(UserContext)
-  const userDate = new Date(user.createdAt).toLocaleDateString()
 
   return (
     <section>
       <p>Name: {user.name}</p>
       <p>Email: {user.email}</p>
-      <p>Created Date: {userDate}</p>
     </section>
   )
 }
