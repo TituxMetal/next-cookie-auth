1f1ecd1264edf96015c1db0abc3ef000425e6e6e
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-06-05 13:26:08 +0200

    Update user routes; add POST /api/users/me route for logout user

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,6 +7,7 @@ const { register, login } = require('../validation')
 
 router.post('/register', validateBody(register), UserController.register)
 router.post('/login', validateBody(login), UserController.login)
+router.post('/me', isAuthenticated, UserController.logout)
 router.get('/me', isAuthenticated, UserController.me)
 
 module.exports = router
