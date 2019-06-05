c4352543ad9dbfe9066743a036050a49f164fe72
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-06-05 07:01:55 +0200

    Update _app.js file; add authStatus props and pass it to ContextProvider component

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,28 @@ import { ThemeProvider } from 'styled-components'
 
 import { ContextProvider } from '../context'
 import { theme } from '../components/styled'
+import { getSessionFromClient, getSessionFromServer } from '../lib'
 
 class MyApp extends App {
   static async getInitialProps ({ Component, router, ctx }) {
     let pageProps = {}
 
+    const authStatus = !ctx.req ? await getSessionFromClient() : await getSessionFromServer(ctx.req)
+
     if (Component.getInitialProps) {
       pageProps = await Component.getInitialProps(ctx)
     }
 
-    return { pageProps }
+    return { pageProps, authStatus }
   }
 
   render () {
-    const { Component, pageProps, router } = this.props
+    const { Component, pageProps, router, authStatus } = this.props
     const { pathname } = router
 
     return (
       <Container>
-        <ContextProvider pathname={pathname}>
+        <ContextProvider pathname={pathname} authStatus={authStatus}>
           <ThemeProvider theme={theme}>
             <Component {...pageProps} />
           </ThemeProvider>
