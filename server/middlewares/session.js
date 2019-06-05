2f1600d72f7da33b4dfd5a35fb2e7c532ecf5a96
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-06-05 02:58:45 +0200

    Add session middleware

---
+++ server/middlewares/session.js
@@ -0,0 +1,15 @@
+const session = require('express-session')
+
+const store = require('../store')
+
+const { isProd, sessName, sessSecret, sessLifetime } = require('../config')
+
+module.exports = session({
+  store,
+  name: sessName,
+  secret: sessSecret,
+  resave: true,
+  rolling: true,
+  saveUninitialized: false,
+  cookie: { maxAge: parseInt(sessLifetime), sameSite: true, secure: isProd }
+})
