337ffe4b763da1a15c01b2879a218f19367e8b7f
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-06-05 02:45:21 +0200

    Update config file; add configuration for Redis and Session

--- server/config/index.js
+++ server/config/index.js
@@ -6,5 +6,13 @@ module.exports = {
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017',
   // JsonWebToken Setup
   jwtSecret: process.env.JWT_SECRET || '$supersecretjsonwebtoken',
-  jwtOptions: { expiresIn: '2 days' }
+  jwtOptions: { expiresIn: 1000 * 60 * 60 * 24 * 2 },
+  // Redis setup
+  redisHost: process.env.REDIS_HOST || 'localhost',
+  redisPort: process.env.REDIS_PORT || 6379,
+  redisPassword: process.env.REDIS_PASSWORD || 'redisDev',
+  // Session setup
+  sessName: process.env.SESS_NAME || 'token',
+  sessSecret: process.env.SESS_SECRET || 'session!secret!',
+  sessLifetime: process.env.SESS_LIFETIME || 1000 * 60 * 60 * 24 * 2 // <- 2 days
 }
